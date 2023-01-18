818fc454a39162b8e0691af3406bf9fd015da2da
Orozkul Basytov <orozkulpeaksoft@gmail.com>
2023-01-18 13:23:03 +0600

    hw-3

--- src/components/expenses/Expenses.jsx
+++ src/components/expenses/Expenses.jsx
@@ -1,9 +1,39 @@
 import { ExpenseItem } from "../expense-item/ExpenseItem"
+import ExpensesFilter from "../ExpenseFilter/ExpensesFilter";
+import { useState } from "react";
+import Chart from "../Chart/Chart";
 
-export const Expenses = ({expenses}) => {
-    return <ul style={{padding: "20px", border: "2px solid " , borderRadius: "13px" , backgroundColor: 'black'}}>
-        {expenses.map((elem) => {
-            return <ExpenseItem title={elem.title} price={elem.price} date={elem.date} />
+export const Expenses = ({ expenses }) => {
+    const [selectedYear, setSelectedYear] = useState("2022");
+
+    const yearChangeHandler = (event) => {
+      setSelectedYear(event.target.value);
+    };
+
+    const filteredItems = expenses.filter((ele) => {
+      const stringifiedYear = new Date(ele.date).getFullYear().toString();
+      return stringifiedYear === selectedYear;
+    });
+    return (
+      <ul
+        style={{
+          padding: "20px",
+          border: "2px solid ",
+          borderRadius: "13px",
+          backgroundColor: "black",
+        }}
+      >
+        <ExpensesFilter/>
+        <Chart expenses={filteredItems} />
+        {filteredItems.map((elem) => {
+          return (
+            <ExpenseItem
+              title={elem.title}
+              price={elem.price}
+              date={elem.date}
+            />
+          );
         })}
-    </ul>
+      </ul>
+    );
 };
